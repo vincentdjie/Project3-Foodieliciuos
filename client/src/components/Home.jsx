37e7a58c742f97797ed662a3ee61f869b0ea0bdf
Vincent Renaldo Djie <daejaeblaq@Vincents-MacBook-Pro.local>
2019-07-22 16:14:51 -0700

    updated

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,20 +5,6 @@ class Home extends React.Component { //component use state
     render() {
         return (
             <>
-            <div className='top-bar'>
-                <div className='logo'>
-                    <img src="" alt=""/>
-                </div>
-
-                <div>
-                    <h2>About</h2>
-                </div>
-
-                <div>
-                    <h2>Menu</h2>
-                </div>
-            </div>
-
             <div className='middle'>
                 <div className='search-elements'>
                     <input type="text" className='search-bar' placeholder='Enter a search keyword...'/>
@@ -27,11 +13,6 @@ class Home extends React.Component { //component use state
                 <div>
                     <button className='submit-button'>Submit</button>
                 </div>
-            </div>
-
-            <div className='bottom-bar'>
-                <div className='dev-info'>Developer Information</div>
-                <div className='licenses'>Licenses</div>
             </div>
             </>
         );
